9df57c3faf1d9454c9bffa31286c6a971dc5999e
Kevin <kevin.06.huang@gmail.com>
2023-01-08 20:20:20 -0800

    Set up express server

---
+++ server/server.js
@@ -0,0 +1,33 @@
+require('dotenv').config();
+
+const path = require('path');
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const { json, urlencoded } = require('express');
+
+app.use(json());
+app.use(urlencoded({ extended: true }));
+
+app.use((err, req, res, next) => {
+  const defaultErr = {
+    log: 'Express error handler caught unknown middleware error',
+    status: 400,
+    message: { err: 'An error occurred' }
+  };
+  const errorObj = Object.assign({}, defaultErr, {message: {err: err}});
+  console.log(`${errorObj.log}`);
+  
+  return res.status(errorObj.status).json(errorObj.message);
+});
+  
+  /**
+   * start server
+   */
+app.listen(PORT, () => {
+  console.log(`Server listening on port: ${PORT}`);
+});
+  
+module.exports = app;
