9800935bc46f2b372b30920334e9028cef84c52c
Kevin <kevin.06.huang@gmail.com>
2023-01-11 01:10:20 -0800

    Pointer

--- client/components/Game.jsx
+++ client/components/Game.jsx
@@ -1,17 +1,19 @@
 import React, { useEffect, useRef, useState, useTexture } from 'react';
 import { Canvas, useFrame } from '@react-three/fiber';
 import { Physics, usePlane, useSphere } from '@react-three/cannon'
-import { OrbitControls, PerspectiveCamera } from '@react-three/drei'
+import { OrbitControls, PerspectiveCamera, PointerLockControls, Sky } from '@react-three/drei'
 
 //import Hall from './Hall';
 import Ground from './Ground';
 import Player from './Player';
 import Vehicle from './Vehicle';
+//<PerspectiveCamera position={[0, 1, 5]} rotation={[0, - 0.5 * Math.PI / 10, 0]} makeDefault/>
 
 const Game = (props) => {
   return (
             <div id="canvas-container" style={{height: window.innerHeight, width: window.innerWidth}}>
             <Canvas>
+                <Sky sunPosition={[100, 20, 100]} />
                 <ambientLight />
                 <pointLight position={[10, 10, 10]} />
                 <spotLight
@@ -25,12 +27,11 @@ const Game = (props) => {
                     }}
                 />
                 <Physics iterations={5} gravity={[0, -5, 0]} >
-                  <PerspectiveCamera position={[0, 1, 5]} rotation={[0, - 0.5 * Math.PI / 10, 0]} makeDefault/>
                   <Player position={[0, 2, 1.5]} />
                   <Ground rotation={[-Math.PI / 2, 0, 0]} userData={{ id: 'floor' }} />
                   <Vehicle position={[3, 3, 0]} rotation={[0, -Math.PI / 4, 0]} angularVelocity={[0, 0.5, 0]} wheelRadius={0.3} />
                 </Physics>
-                <OrbitControls/>
+                <PointerLockControls />
             </Canvas>
             </div>
         );
---
+++ client/components/Player2.jsx
@@ -0,0 +1,40 @@
+import React, { Suspense, useRef, useState } from 'react';
+import { Physics, usePlane, useSphere } from '@react-three/cannon'
+import { useFrame } from '@react-three/fiber';
+import { useLoader } from '@react-three/fiber';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import { useGLTF, useTexture } from '@react-three/drei';
+
+const Player = (props) => {
+    // This reference gives us direct access to the THREE.Mesh object
+    //const ref = useRef()
+    const [ref] = useSphere(
+        () => ({ args: [0.75], mass: 1, ...props }),
+        useRef()
+      )
+    // Hold state for hovered and clicked events
+   // const [hovered, hover] = useState(false)
+    const [clicked, click] = useState(false)
+    // Subscribe this component to the render-loop, rotate the mesh every frame
+    useFrame((state, delta) => (ref.current.rotation.x += delta))
+    // Return the view, these are regular Threejs elements expressed in JSX
+   // const gltf = useLoader(GLTFLoader, '../assets/darth_vader.gltf')
+    /*return (
+        <Suspense fallback={null}>
+          <primitive object={gltf.scene} />
+        </Suspense>
+    )*/
+
+    
+    return (
+      <mesh
+        {...props}
+        ref={ref}>
+        <sphereGeometry args={[0.75]} geometry={[0, 0, 0]} material={ {color: 0xffff00 }}/>
+        <meshPhysicalMaterial map={useTexture("../assets/Cs5Cn-GUAAA-uJM.jpeg")} transmission={0} roughness={0} thickness={10} envMapIntensity={1} />
+      </mesh>
+    )
+  }
+
+  export default Player;
+  //useGLTF.preload('../assets/darth_vader.gltf')
